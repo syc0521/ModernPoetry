17ee451f226e37395cfa6f3edaa7b1a04c566827
syc0521 <65008252+syc0521@users.noreply.github.com>
2020-07-30 22:13:28 +0800

    add puzzle

---
+++ Assets/Scripts/Puzzle/CreatePatch.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Games.Puzzle
+{
+	public class CreatePatch : MonoBehaviour
+	{
+		public Sprite[] spritePatches;
+        private GameObject[,] patches;
+		public Vector2 patchXRange, patchYRange;
+
+        public int ColNumber { get; } = 3;
+
+        public int RowNumber { get; } = 3;
+
+        void Start()
+		{
+			patches = new GameObject[ColNumber, RowNumber];
+			spritePatches = Resources.LoadAll<Sprite>("Patches");
+			CreatePatchObj(spritePatches);
+		}
+
+		void Update()
+		{
+
+		}
+
+		private void CreatePatchObj(Sprite[] sprites)
+		{
+			for (int row = 0; row < RowNumber; row++)
+			{
+				for (int col = 0; col < ColNumber; col++)
+				{
+					patches[row, col] = new GameObject("patch" + row + col);
+					patches[row, col].AddComponent<SpriteRenderer>().sprite = sprites[row * ColNumber + col];
+					patches[row, col].GetComponent<SpriteRenderer>().sortingOrder = 2;
+					float x = Random.Range(patchXRange.x, patchXRange.y);
+					float y = Random.Range(patchYRange.x, patchYRange.y);
+					patches[row, col].GetComponent<Transform>().position = new Vector3(x, y, 0);
+					Vector2 patchSize = patches[row, col].GetComponent<SpriteRenderer>().sprite.bounds.size;
+					Vector3 initPosition = patches[row, col].GetComponent<Transform>().position;
+					Vector3 targetPosition = new Vector3((col + 0.5f) * patchSize.x, 
+														 (RowNumber - row - 0.5f) * patchSize.y, 0);
+					patches[row, col].AddComponent<Patch>().InitPatch(patchSize.x, patchSize.y, targetPosition, initPosition);
+					//patches[row, col].GetComponent<Transform>().position = patches[row, col].GetComponent<Patch>().GetTargetPosition();
+				}
+			}
+		}
+		public GameObject[,] GetPatchObj()
+        {
+			return patches;
+        }
+	}
+
+}
---
+++ Assets/Scripts/Puzzle/Patch.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Games.Puzzle
+{
+	public class Patch : MonoBehaviour
+	{
+		private Vector3 targetPosition;
+		private Vector3 initPosition;
+		private float PatchWidth;
+		private float PatchHeight;
+
+		public void InitPatch(float PatchWidth,float PatchHeight,Vector3 targetPosition, Vector3 initPosition)
+		{
+			this.PatchWidth = PatchWidth;
+			this.PatchHeight = PatchHeight;
+			this.targetPosition = targetPosition;
+			this.initPosition = initPosition;
+		}
+		public Vector3 GetTargetPosition()
+        {
+			return targetPosition;
+        }
+		public float GetWidth()
+        {
+			return PatchWidth;
+        }
+		public float GetHeight()
+        {
+			return PatchHeight;
+        }
+	}
+
+}
