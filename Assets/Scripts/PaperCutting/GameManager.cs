aedaa99d78ed08a7608365f6be9fecb0ce3e7a18
syc0521 <65008252+syc0521@users.noreply.github.com>
2020-08-28 16:12:38 +0800

    initialize paper

---
+++ Assets/Scripts/PaperCutting/GameManager.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public GameObject[] papers = new GameObject[3];
+    public GameObject[] paperButton = new GameObject[3];
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
